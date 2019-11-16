2edae6cffb216fef736d16a1fd423bf2f9cbdb1f
CamilleLP4 <camille.paudrat.lp4@gmail.com>
2019-11-16 20:28:46 +0100

    Commit Final

--- HackerTracker.java
+++ HackerTracker.java
@@ -21,30 +21,36 @@ public class HackerTracker {
     /**
      * Constructeur
      */
-    public HackerTracker() {}
+    public HackerTracker() {
+    }
 
     /**
      * Lit le fichier fileName, le stock dans une liste et transforme la list
+     * 
      * @param fileName le fichier connexion a lire
      * @return une liste de tableau de chaine de caractere transforme
      * @throws IOException
      */
     private List<String[]> readLog(String fileName) throws IOException {
-        List<String> read = Files.readAllLines(Paths.get(fileName)); // lit toutes les lignes du fichier et les stocks dans une liste
+        List<String> read = Files.readAllLines(Paths.get(fileName)); // lit toutes les lignes du fichier et les stocks
+                                                                     // dans une liste
         return this.transformList(read);
     }
 
     /**
      * Lit le fichiers warning et le stock dans une liste
+     * 
      * @return une liste de chaine de caractere
      * @throws IOException
      */
     private List<String> readWarningLog() throws IOException {
-        return Files.readAllLines(Paths.get(this.warning)); // lit toutes les lignes du fichier et les stocks dans une liste
+        return Files.readAllLines(Paths.get(this.warning)); // lit toutes les lignes du fichier et les stocks dans une
+                                                            // liste
     }
 
     /**
      * Prepare l'ecriture du fichier
+     * 
      * @param fileName le fichier a ecrire
      * @return le buffer pour ecrire
      * @throws IOException
@@ -61,47 +67,71 @@ public class HackerTracker {
      * @throws IOException
      */
     private void closeBuffer() throws IOException {
-        this.buffer.close();
+        this.buffer.close(); // ferme le buffer
     }
 
     /**
-     * Transforme la liste en effectuant un split pour preparer le contenu du fichier
+     * Transforme la liste en effectuant un split pour preparer le contenu du
+     * fichier
+     * 
      * @param list le contenu brut du fichier
      * @return une liste de tableau de chaine de caractere split
      */
     private List<String[]> transformList(List<String> list) {
         List<String[]> listTransform = new ArrayList<>();
         for (String string : list) {
-            listTransform.add(string.split(";"));
+            listTransform.add(string.split(";")); // decoupe la liste au ;
         }
         return listTransform;
     }
 
     /**
-     * Travaille le parametre pour extraire l'heure et verifie si elle est dans les heures de travail
+     * Travaille le parametre pour extraire l'heure et verifie si elle est dans les
+     * heures de travail
+     * 
      * @param heure date complet a formater
      * @return true si l'heure est hors des heures de travail
      */
     private boolean tryHour(String heure) {
-        String[] splitSpace = heure.split(" ");
-        String[] splitHour = splitSpace[1].split(":");
-        int hour = Integer.parseInt(splitHour[0]);
+        String[] splitSpace = heure.split(" "); // decoupe la chaine de caractere aux espaces
+        String[] splitHour = splitSpace[1].split(":"); // decoupe la chaine de caractere deja coupe au :
+        int hour = Integer.parseInt(splitHour[0]); // transforme une partie de la chaine en int
         return (8 > hour || hour > 19);
     }
 
     /**
      * Ecrit dans la memoire tampon
+     * 
      * @param aInserer chaine a caracter a ajouter
-     * @param buffer le buffer
+     * @param buffer   le buffer
      * @throws IOException
      */
     private void write(String aInserer) throws IOException {
-        this.buffer.write(aInserer);
-        this.buffer.newLine();
+        this.buffer.write(aInserer); // insere la chiane de caractere dans le buffer
+        this.buffer.newLine(); // insere un saut de ligne
     }
 
     /**
-     * Execute les differentes fonctions permettant de creer et remplir le fichier avec les identififants de connexion
+     * Compare la chaine de caractere en parametre avec la list forbidden
+     * 
+     * @param prohibited IP a comparer
+     * @return true si prohibited correspond a une des IP warning
+     * @throws IOException
+     */
+    public boolean warningLog(String prohibited) throws IOException {
+        List<String> forbidden = this.readWarningLog();
+        for (String stringforbidden : forbidden) {
+            if (stringforbidden.equals(prohibited)) { // si l'adresse IP fait partie de celles interdites
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Execute les differentes fonctions permettant de creer et remplir le fichier
+     * avec les identififants de connexion
+     * 
      * @param fileName le fichier connexion a lire
      * @throws IOException
      */
@@ -111,20 +141,23 @@ public class HackerTracker {
         this.openWriteLog(newFile);
         List<String> users = new ArrayList<>();
         for (String[] strings : list) {
-            users.add(strings[1]);      
+            users.add(strings[1]); // stocke dans une liste le nom d'utilisateur
         }
         Set<String> set = new HashSet<>();
-        set.addAll(users);
+        set.addAll(users); // elimine les doublons de la liste
         for (String string : set) {
-            this.write(string);
+            this.write(string); // parcours la liste sans doublon et l'insere dans le buffer
         }
         this.closeBuffer();
     }
 
     /**
-     * Execute les differentes fonctions permettant de creer et remplir le fichier avec les connexions hors des horaires de travail et les connexion depuis des IP interdites
+     * Execute les differentes fonctions permettant de creer et remplir le fichier
+     * avec les connexions hors des horaires de travail et les connexion depuis des
+     * IP interdites
+     * 
      * @param fileName le fichier connexion a lire
-     * @param warning le fichier warning a lire
+     * @param warning  le fichier warning a lire
      * @throws IOException
      */
     public void suspectLog(String fileName, String warning) throws IOException {
@@ -136,50 +169,39 @@ public class HackerTracker {
         this.write("Connexion Hors des Heures de travail");
         for (String[] strings : list) {
             if (this.tryHour(strings[2])) {
-                this.write("Utilisateurs : " + strings[1] + " IP : " + strings[0]);
+                this.write("Utilisateurs : " + strings[1] + " IP : " + strings[0]); // insere dans le buffer
+                                                                                    // l'utilisateur et l'ip si c'est
+                                                                                    // hors horaire
             }
-            if (this.warningLog(strings[0])) {
+            if (this.warningLog(strings[0])) { // si l'ip correspond a une ip interdite
                 if (suspect.containsKey(strings[1])) {
-                    suspect.get(strings[1]).add(strings[0]);
+                    suspect.get(strings[1]).add(strings[0]); // si l'utilisateur est deja dans la HashMap on ajoute l'ip
+                                                             // a la liste
                 } else {
                     List<String> newList = new ArrayList<>();
                     newList.add(strings[0]);
-                    suspect.put(strings[1], newList);
+                    suspect.put(strings[1], newList); // sinon on le crée dans la HashMap et on y ajoute la liste
                 }
-                
+
             }
         }
-        buffer.newLine();
+        this.write("");
         this.write("Connexion depuis IP interdites");
         for (String key : suspect.keySet()) {
-            this.write(key + ";" + suspect.get(key).size());
+            this.write(key + ";" + suspect.get(key).size()); // insere dans le buffer l'utilisateur et le taille de la
+                                                             // liste des IP interdites utilise
         }
         this.closeBuffer();
     }
 
-    /**
-     * 
-     * @param prohibited IP a comparer
-     * @return true si prohibited correspond a une des IP warning
-     * @throws IOException
-     */
-    public boolean warningLog(String prohibited) throws IOException {
-        List<String> forbidden = this.readWarningLog();
-        for (String stringforbidden : forbidden) {
-            if (stringforbidden.equals(prohibited)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     public static void main(String[] args) throws IOException {
-        System.out.println("Debut de la recherche.\n");
         HackerTracker track = new HackerTracker();
         String fileName = "connexion.log";
         String warning = "warning.txt";
+
+        System.out.println("\nDebut de la recherche.");
         track.userLog(fileName);
         track.suspectLog(fileName, warning);
-        System.out.println("\nFin du programme.");
+        System.out.println("Fin de la recherche.");
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,93 @@
-# hackLaPoste
-Projet hackLaPoste
+# Projet Hack à La Poste
+
+Auteur : Camille
+
+Date de début : 06/11/19
+
+Contexte : Projet Java 3 Hack a La Poste
+
+Développé en Java
+
+## Description générale
+
+Le programme lit et traite 2 fichiers pour extraire les utilisateurs, les connexions hors des horaires et les connexions depuis des IP interdites
+
+## Description des méthodes
+
+La classe possède 2 attributs :
+* *warning* Chaine de caractère privé
+* *buffer* Memoire tampon d'ecriture privé (BufferedWriter)
+
+### readLog()
+
+La méthode prend un paramètre *fileName*, envoie des exceptions et retourne une liste de tableau de chaine de caractère.
+La méthode lit le fichier *fileName* et stocke toutes les lignes du fichier dans une liste de tableau de chaine de caractère.
+
+### readWarningLog()
+
+La méthode ne prend pas de paramètre, envoie des exceptions et retourne une liste de tableau de chaine de caractère.
+La méthode lit l'attribut *warning* et stocke toutes les lignes du fichier dans une liste de tableau de chaine de caractère.
+
+### openWriteLog()
+
+La méthode prend un paramètre *fileName*, envoie des exceptions et ne retourne rien.
+La méthode prépare le fichier à écrire et remplit l'attribut *buffer*.
+
+### readLog()
+
+La méthode prend un paramètre *fileName*, envoie des exceptions et retourne une liste de tableau de chaine de caractère.
+La méthode lit le fichier *fileName* et stocke toutes les lignes du fichier dans une liste.
+
+### closeBuffer()
+
+La méthode ne prend pas de paramètre, envoie des exceptions et ne retourne rien.
+La méthode ferme le *buffer*.
+
+### transformList()
+
+La méthode prend un paramètre *list*, envoie des exceptions et retourne une liste de tableau de chaine de caractère.
+La méthode transforme *list* en découpant les chaines de caractères contenu dans la liste à chaque *;*.
+
+### tryHour()
+
+La méthode prend un paramètre *heure* et retourne un boolean.
+La méthode transforme *heure* en le découpant 2 fois aux *espaces* puis aux *:*.
+Le résultat est transforme en nombre puis testé pour savoir si il est dans les horaires de travail ou non.
+
+### write()
+
+La méthode prend un paramètre *aInserer*, envoie des exceptions et ne retourne rien.
+Elle ajoute a la mémoire tampon le contenu de *aInserer* puis un saut de ligne.
+
+### warningLog()
+
+La méthode prend un paramètre *prohibited*, envoie des exceptions et retourne un boolean.
+Elle compare *prohibited* avec la liste de chaine de caractère *forbidden* et retourne *true* si elle correspond. 
+
+### userLog()
+
+La méthode prend un paramètre *fileName*, envoie des exceptions et ne retourne rien.
+Elle exécute les différentes méthodes permettant la création et le remplissage de *utilisateurs.txt*.
+La méthode fait :
+* readLog()
+* openWriteLog()
+* Lit le résultat de readLog() et recupère le nom d'utilisateur
+* Elimine les doublons de la liste d'utilisateur
+* Ajoute chaque utilisateur de la liste sans doublon dans la memoire tampon
+* closeBuffer()
+
+### suspectLog()
+
+La méthode prend deuc paramètre *fileName* et *warning*, envoie des exceptions et ne retourne rien.
+Elle exécute les différentes méthodes permettant la création et le remplissage de *suspect.txt*.
+La méthode fait :
+* readLog()
+* openWriteLog()
+* Lit le résultat de readLog() remplit la memoire tampon avec les utilisateur se connectant hors horaires de travail
+* Remplit une memoire de données si l'utilisateur se connecte depuis une IP interdite
+* Ajoute le contenu de la memoire de données dans la memoire tampon
+* closeBuffer()
+
+## Notice
+
+Executer un Run sur la Classe HackerTracker.java
